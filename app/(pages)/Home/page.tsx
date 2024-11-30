ac3a7e5694fb956c758daf9d6d669bfd14ca80ff
jirawat <66022477@up.ac.th>
2024-11-30 23:58:28 +0700

    sddd

--- app/(pages)/Home/page.tsx
+++ app/(pages)/Home/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         height={400} 
       /></div>
       <div className="flex flex-col items-center justify-center">
-        <a className="text-[1.5rem] text-[#00000080]">{" Hello ,I'm"}</a>
+        <a className="text-[1.5rem] text-[#00000080]">{" Hello ,I&aposm"}</a>
         <a className="text-[3rem] text-black font-semibold"> Mack JR </a>
         <div className="text-[#000000ad] text-[1.75rem] mb-[10]">UX/UI Design</div>
         <div className="flex text-black">
