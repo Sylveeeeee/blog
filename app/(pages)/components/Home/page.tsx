8bc15365dd179848f3457dc2b2e3c4fdd85be31d
jirawat <66022477@up.ac.th>
2025-01-23 23:42:11 +0700

    dd

--- app/(pages)/components/Home/page.tsx
+++ app/(pages)/components/Home/page.tsx
@@ -7,12 +7,12 @@ export default function Home() {
   return (
     <>
     <div id="home" className="flex justify-center items-center h-[80vh] gap-[5rem] ">
-      <div className="rounded-full h-[400] w-[400] overflow-hidden">
+      <div className="rounded-full h-[400] w-[400] overflow-hidden flex justify-center ">
       <ImageComponent 
-        src="/Profile2.png" 
+        src="/ddd.jpg" 
         alt="Profile" 
         width={400} 
-        height={400} 
+        height={600} 
       /></div>
       <div className="flex flex-col items-center justify-center">
         <p className="text-[1.5rem] text-[#00000080]">{` Hello ,I'm`}</p>
