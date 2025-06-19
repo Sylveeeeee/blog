80e80059c391e3bc424888e91111e83bea1341c3
jirawat <66022477@up.ac.th>
2025-06-19 15:56:46 +0700

    project page

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -4,14 +4,13 @@ import Contact from "./components/Contact/page";
 import Home from "./components/Home/page";
 import ScrollToTopButton from "./components/ScrollToTopButton";
 import Footer from "./components/footer/page";
-import { Project } from "./components/Projects/page";
-
+import Projects from "./components/Projects/page";
 export default function page() {
   return (
     <>
     <Home/>
     <About/>
-    <Project/>
+    <Projects/>
     <Experience/>
     <Contact/>
     <ScrollToTopButton/>
